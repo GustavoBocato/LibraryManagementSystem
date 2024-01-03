89ed58b61a64e50c8e4dfab0e843cc8a6665e4a0
PC <gustavosbocato@gmail.com>
2024-01-03 20:22:54 -0300

    aa

---
+++ src/Cliente.java
@@ -0,0 +1,21 @@
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String endereço;
+
+    public Cliente(String nome, String cpf, String telefone, String email, String endereço){
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereço = endereço;
+
+    }
+
+
+
+}
