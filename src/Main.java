94507857826b5aedb8e9085211c92525cdf79e1d
PC <gustavosbocato@gmail.com>
2024-01-25 16:37:46 -0300

    Apagando um import desnecessario.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.util.Scanner;
 import java.sql.Statement;
 
 public class Main {
